adf2bbfd58ab5345b4375cf6f21200bf1aa4d4ec
tmack4444 <>
2021-04-05 14:38:48 -0400

    Updated codea  bit, it now actually updates the number of bytes read and written during use. Simply have to keep creating an object Disks to get updated values. I tested that it was updating by downloading a file while running the program

--- src/main/java/CRUM.java
+++ src/main/java/CRUM.java
@@ -3,14 +3,35 @@ import oshi.hardware.HWDiskStore;
 import oshi.hardware.HardwareAbstractionLayer;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class CRUM {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
         SystemInfo si = new SystemInfo();
         HardwareAbstractionLayer hal = si.getHardware();
-        List<HWDiskStore> Disks = new List<HWDiskStore>();
+        List<HWDiskStore> disks = hal.getDiskStores();
+        for(int i = 0; i < disks.size(); i++){
+            HWDiskStore disk = disks.get(i);
+            System.out.println();
+            System.out.println("disk name: " + disk.getName());
+            System.out.println("disk model: " + disk.getModel());
+            System.out.println("disk size: " + (disk.getSize() / 1073741824) + " GB");
+        }
+        while(true){
+            for(int i = 0; i < disks.size(); i++){
+                List<HWDiskStore> Disks = hal.getDiskStores();
+                HWDiskStore disk = Disks.get(i);
+                System.out.println();
+                System.out.println(disk.getName());
+                System.out.println("Reads: " + disk.getReads());
+                System.out.println("Bytes read: " + disk.getReadBytes());
+                System.out.println("Writes: " + disk.getReads());
+                System.out.println("Bytes written: " + disk.getReadBytes());
+                System.out.println("Time in use: " + disk.getTransferTime());
+            }
+            TimeUnit.SECONDS.sleep(1);
+        }
 
-        System.out.println("hal: " + hal);
     }
 }
