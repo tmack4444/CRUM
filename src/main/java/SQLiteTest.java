58c7083c1aa2305cb5ffb745046f9630dfd4d02e
Samantha Berry <Samantha.Berry1@marist.edu>
2021-04-09 18:00:09 -0400

    added more to SQLiteTest

--- src/main/java/SQLiteTest.java
+++ src/main/java/SQLiteTest.java
@@ -11,13 +11,26 @@ public class SQLiteTest {
             System.out.println("Opened database successfully");
 
             stmt = c.createStatement();
-            String sql = "CREATE TABLE TESTDISC " +
-                    "(ID INT PRIMARY KEY     NOT NULL," +
-                    " NAME           TEXT    NOT NULL, " +
-                    " AGE            INT     NOT NULL, " +
-                    " ADDRESS        CHAR(50), " +
-                    " SALARY         REAL)";
-            stmt.executeUpdate(sql);
+            String sql_machine = "CREATE TABLE IF NOT EXISTS TESTMACHINE " +
+                    "(MACHINE_ID INT      NOT NULL," +
+                    "TIMESTAMP DATETIME NOT NULL," +
+                    " MACHINE_MODEL           TEXT    NOT NULL, " +
+                    " MACHINE_VENDOR      TEXT     NOT NULL, " +
+                    "PRIMARY KEY(MACHINE_ID, TIMESTAMP))";
+
+            stmt.executeUpdate(sql_machine);
+
+            String sql_disc = "CREATE TABLE IF NOT EXISTS TESTDISC " +
+                    "(DISC_ID INT      NOT NULL," +
+                    "MACHINE_ID INT      NOT NULL," +
+                    "TIMESTAMP DATETIME NOT NULL," +
+                    " DISC_NAME           TEXT    NOT NULL, " +
+                    " DISC_MODEL          TEXT     NOT NULL, " +
+                    " DISC_SIZE        INT    NOT NULL, " +
+                    "DISC_USEd        INT   NOT NULL," +
+                    "PRIMARY KEY(DISC_ID, MACHINE_ID, TIMESTAMP)," +
+                    "FOREIGN KEY(MACHINE_ID) REFERENCES TESTMACHINE(MACHINE_ID))";
+            stmt.executeUpdate(sql_disc);
             stmt.close();
             c.close();
         } catch ( Exception e ) {
