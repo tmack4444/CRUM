41a4c52e372abe763447c17bae3b7fb3dd62d0e8
Paul Ippolito <paul.ippolito2@marist.edu>
2021-04-16 14:10:53 -0400

    Base tabs created, Disk display labels added. Main UI base set up.

--- src/main/java/CrumUI.java
+++ src/main/java/CrumUI.java
@@ -3,5 +3,12 @@ import javax.swing.*;
 public class CrumUI {
     private JTabbedPane tabbedPane1;
     private JPanel rootPanel;
-    private JPanel diskPanel;
+    private JPanel CpuPanel;
+    private JPanel RamPanel;
+    private JPanel NetworkPanel;
+    private JPanel DiskPanel;
+    private JLabel readMetrics;
+    private JLabel readLabel;
+    private JLabel writeLabel;
+    private JLabel writeMetrics;
 }
