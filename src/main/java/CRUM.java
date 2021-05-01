33f5371ba6fa385b2dd64f2874203b2ba11a7a93
tmack4444 <>
2021-05-01 16:31:41 -0400

    Ok think I'm passing the right data into the testdisc db. I need to setup a function to get the information just to make sure it's actually running these queries, but I think this is just about finished, and ready to merge with the disk branch

--- src/main/java/CRUM.java
+++ src/main/java/CRUM.java
@@ -33,39 +33,18 @@ public class CRUM {
 
                     List<HWDiskStore> Disks = hal.getDiskStores();
                     HWDiskStore disk = Disks.get(i);
-                    int usedSpace = disk.getSize() - disk.getWriteBytes();
-                    String sql_mach_insert = "INSERT INTO TESTDISC VALUES(?,?,?,?,?,?,?,?,?,?");
-                    String sql_disc = "CREATE TABLE IF NOT EXISTS TESTDISC " +
-                            "(DISC_ID INT      NOT NULL," +
-                            "MACHINE_ID INT      NOT NULL," +
-                            "TIMESTAMP TIMESTAMP NOT NULL," +
-                            " DISC_NAME           TEXT    NOT NULL, " +
-                            " DISC_MODEL          TEXT     NOT NULL, " +
-                            " DISC_SIZE        INT    NOT NULL, " +
-                            "DISC_USED        INT   NOT NULL," +
-                            "DISC_SPEED INT NOT NULL, " +
-                            "PRIMARY KEY(DISC_ID, MACHINE_ID, TIMESTAMP)," +
-                            "FOREIGN KEY(MACHINE_ID) REFERENCES TESTMACHINE(MACHINE_ID))";
-                    stmt.executeUpdate(sql_disc);
+                    int usedSpace = (int) (disk.getSize() - disk.getWriteBytes());
+                    String sql_mach_insert = "INSERT INTO TESTDISC VALUES(?,?,?,?,?,?,?,?)";
                     PreparedStatement smi = c.prepareStatement(sql_mach_insert);
                     smi.setInt(1, i);
-                    smi.setInt(2, SerialNum);
-                    smi.setTimestamp(3, calendar.getTime().getTime());
+                    smi.setString(2, SerialNum);
+                    smi.setTimestamp(3, currentTime);
                     smi.setString(4, disk.getName());
                     smi.setString(5, disk.getModel());
-                    smi.setInt(6, disk.getSize());
+                    smi.setLong(6, disk.getSize());
                     smi.setInt(7, usedSpace);
-                    smi.setInt(8, disk.getTransferTime());
+                    smi.setLong(8, disk.getTransferTime());
                     smi.execute();
-                    /*
-                    System.out.println();
-                    System.out.println(disk.getName());
-                    System.out.println("Reads: " + disk.getReads());
-                    System.out.println("Bytes read: " + disk.getReadBytes());
-                    System.out.println("Writes: " + disk.getReads());
-                    System.out.println("Bytes written: " + disk.getReadBytes());
-                    System.out.println("Time in use: " + disk.getTransferTime());
-                     */
                 }
                 TimeUnit.SECONDS.sleep(1);
             }
