9d1953d2ddc8b63080dd924498d95ca8d69311aa
Paul Ippolito <paul.ippolito2@marist.edu>
2021-05-03 14:52:22 -0400

    Realized I don't need multiple labels, one as a title, other for actual data. can be condensed to one

--- src/main/java/CrumUI.java
+++ src/main/java/CrumUI.java
@@ -7,22 +7,18 @@ public class CrumUI {
     private JPanel RamPanel;
     private JPanel NetworkPanel;
     private JPanel DiskPanel;
-    private JLabel readMetrics;
-    private JLabel readLabel;
-    private JLabel writeLabel;
-    private JLabel writeMetrics;
+    private JLabel readSpeed;
+    private JLabel writeSpeed;
     private JLabel bytesRead;
-    private JLabel readBytesNum;
     private JLabel bytesWritten;
-    private JLabel writeBytesNum;
 
     public static void main(String[] args){
         JFrame frame = new JFrame("C.R.U.M");
 
-        frame.setSize(700,700);
+        //frame.setSize(500,500);
         frame.setContentPane(new CrumUI().rootPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //frame.pack();
+        frame.pack();
         //centers the window
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
