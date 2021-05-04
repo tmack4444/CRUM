c439fe4f5a7ba48df7228db2ea864b24b4f9b979
Paul Ippolito <paul.ippolito2@marist.edu>
2021-05-04 14:52:07 -0400

    Dynamic tab creation base successfully implemented. UI can create/add tabs in constructor before initial display

--- src/main/java/CrumUI.java
+++ src/main/java/CrumUI.java
@@ -7,20 +7,31 @@ public class CrumUI extends JFrame {
     private JPanel CpuPanel;
     private JPanel RamPanel;
     private JPanel NetworkPanel;
-    private JPanel DiskPanel;
+    private JPanel DiskPane;
     private JLabel readSpeed;
     private JLabel writeSpeed;
     private JLabel bytesRead;
     private JLabel bytesWritten;
 
-    public static void main(String[] args){
-        JFrame frame = new JFrame("C.R.U.M");
+    /**
+     * This constructor method also handles
+     * dynamic tab creation (mainly used for handling
+     * multiples of a hardware component, like Disk.
+     * Any additional tabs will be added within here,
+     * as the constructor has access to tabbedPane1
+     * @param title
+     */
+    public CrumUI(String title){
+        super(title);
 
-        //frame.setSize(500,500);
-        frame.setContentPane(new CrumUI().rootPanel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        //centers the window
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(rootPanel);
+        this.tabbedPane1.addTab("Disk:0", new DiskPanel());
+        this.pack();
+    }
+
+    public static void main(String[] args){
+        JFrame frame = new CrumUI("C.R.U.M");
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
