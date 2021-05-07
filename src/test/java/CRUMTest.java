f60f72d4b327cb5b5f77b70e87a2d3300c2fbdd8
tmack4444 <>
2021-05-07 16:59:51 -0400

    Started working on adding getDiskDataTest, and added a test.txt. Just need to get starting values for reads and writes from dB, and final values for reads and writes after reading/writing file test.txt and test2.txt

--- src/test/java/CRUMTest.java
+++ src/test/java/CRUMTest.java
@@ -2,6 +2,12 @@ import oshi.SystemInfo;
 import oshi.hardware.HWDiskStore;
 import oshi.hardware.HardwareAbstractionLayer;
 
+import java.io.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Calendar;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,11 +15,8 @@ import org.junit.jupiter.api.Test;
 
 
 public class CRUMTest {
-    private static List<HWDiskStore> disks;
-    private static SystemInfo si;
-    private static HardwareAbstractionLayer hal;
-    private static String SerialNum;
-    public static int numDisks;
+    static Connection c = null;
+    static Statement stmt = null;
 
   @Test
   void initOSHITest(){
@@ -25,4 +28,31 @@ public class CRUMTest {
       assertNotNull(crum.SerialNum);
   }
 
+  @Test
+  void getDiskDataTest(){
+      CRUM crum = new CRUM();
+      crum.initOSHI();
+      Calendar calendar = Calendar.getInstance();
+      crum.getDiskData(calendar);
+      String sourceFile = "test.txt";
+      crum.getDiskData(calendar);
+      try {
+          String sql_Search = "SELECT * FROM TESTMACHINE";
+          ResultSet rs = stmt.executeQuery(sql_Search);
+          int length;
+          byte[] buffer = new byte[1024];
+          OutputStream os = new FileOutputStream("test2.txt");
+          InputStream is = new FileInputStream(sourceFile);
+          while((length = is.read(buffer)) > 0) {
+              os.write(buffer, 0, length);
+          }
+      } catch (FileNotFoundException e) {
+          e.printStackTrace();
+      } catch (IOException e) {
+          e.printStackTrace();
+      } catch (SQLException throwables) {
+          throwables.printStackTrace();
+      }
+      crum.getDiskData(calendar);
+  }
 }
