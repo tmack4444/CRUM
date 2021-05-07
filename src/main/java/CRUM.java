cecf172cd1ca1b4b9e1d63f79de17296beac48f9
tmack4444 <>
2021-05-06 20:38:01 -0400

    Filled in initOSHI and get diskData. This is the basis for how other resources will be gathered, write a separate function to get the information from OSHI and do all DB queries in that function. Now that it's split up, I just have to add javadoc and unit tests by this weekend. Other features will come before next weekend

--- src/main/java/CRUM.java
+++ src/main/java/CRUM.java
@@ -9,20 +9,19 @@ import java.util.concurrent.TimeUnit;
 
 public class CRUM {
     static Logger LOGGER = LoggerFactory.getLogger(CRUM.class);
+    private static List<HWDiskStore> disks;
+    private static SystemInfo si;
+    private static HardwareAbstractionLayer hal;
+    private static String SerialNum;
     public static int numDisks;
     static Connection c = null;
     static Statement stmt = null;
 
     public static void main(String[] args) throws InterruptedException {
-        //
 
-        try {
+
             initDB();
-            SystemInfo si = new SystemInfo();
-            HardwareAbstractionLayer hal = si.getHardware();
-            String SerialNum = hal.getComputerSystem().getSerialNumber();
-            List<HWDiskStore> disks = hal.getDiskStores();
-            numDisks = disks.size();
+            initOSHI();
             CrumUI.createUI();
             for(int i = 0; i < disks.size(); i++){
                 HWDiskStore disk = disks.get(i);
@@ -32,38 +31,10 @@ public class CRUM {
                 System.out.println("disk size: " + (disk.getSize() / 1073741824) + " GB");
             }
             while(true){
-                for(int i = 0; i < disks.size(); i++) {
-                    Calendar calendar = Calendar.getInstance();
-                    java.sql.Timestamp currentTime = new java.sql.Timestamp(calendar.getTime().getTime());
-                    List<HWDiskStore> Disks = hal.getDiskStores();
-                    HWDiskStore disk = Disks.get(i);
-                    LOGGER.info("Disk:  {}", disk.getName());
-                    LOGGER.info("Reads:  {}", disk.getReads());
-                    LOGGER.info("Bytes read: {} GB", disk.getReadBytes());
-                    LOGGER.info("Writes:  {}", disk.getWrites());
-                    LOGGER.info("Bytes written: {} GB", disk.getWriteBytes());
-                    LOGGER.info("Time in use: {} \n", disk.getTransferTime());
-                    int usedSpace = (int) (disk.getSize() - disk.getWriteBytes());
-                    String sql_mach_insert = "INSERT INTO DISC VALUES(?,?,?,?,?,?,?,?)";
-                    PreparedStatement smi = c.prepareStatement(sql_mach_insert);
-                    smi.setInt(1, i);
-                    smi.setString(2, SerialNum);
-                    smi.setTimestamp(3, currentTime);
-                    smi.setString(4, disk.getName());
-                    smi.setString(5, disk.getModel());
-                    smi.setLong(6, disk.getSize());
-                    smi.setInt(7, usedSpace);
-                    smi.setLong(8, disk.getTransferTime());
-                    smi.execute();
-
-                }
+                Calendar calendar = Calendar.getInstance();
+                getDiskData(calendar);
                 TimeUnit.SECONDS.sleep(1);
             }
-        } catch ( Exception e ) {
-            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-            System.exit(0);
-        }
-
     }
 
     public static void initDB(){
@@ -112,10 +83,42 @@ public class CRUM {
     }
 
     public static void initOSHI(){
-
+        si = new SystemInfo();
+        hal = si.getHardware();
+        SerialNum = hal.getComputerSystem().getSerialNumber();
+        disks = hal.getDiskStores();
+        numDisks = disks.size();
     }
 
-    public static void getDiskData(){
+    public static void getDiskData(Calendar calendar){
+        try {
+          for(int i = 0; i < disks.size(); i++) {
+            java.sql.Timestamp currentTime = new java.sql.Timestamp(calendar.getTime().getTime());
+            List<HWDiskStore> Disks = hal.getDiskStores();
+            HWDiskStore disk = Disks.get(i);
+            LOGGER.info("Disk:  {}", disk.getName());
+            LOGGER.info("Reads:  {}", disk.getReads());
+            LOGGER.info("Bytes read: {} GB", disk.getReadBytes());
+            LOGGER.info("Writes:  {}", disk.getWrites());
+            LOGGER.info("Bytes written: {} GB", disk.getWriteBytes());
+            LOGGER.info("Time in use: {} \n", disk.getTransferTime());
+            int usedSpace = (int) (disk.getSize() - disk.getWriteBytes());
+            String sql_mach_insert = "INSERT INTO DISC VALUES(?,?,?,?,?,?,?,?)";
+            PreparedStatement smi = c.prepareStatement(sql_mach_insert);
+            smi.setInt(1, i);
+            smi.setString(2, SerialNum);
+            smi.setTimestamp(3, currentTime);
+            smi.setString(4, disk.getName());
+            smi.setString(5, disk.getModel());
+            smi.setLong(6, disk.getSize());
+            smi.setInt(7, usedSpace);
+            smi.setLong(8, disk.getTransferTime());
+            smi.execute();
+          }
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
 
     }
 }
