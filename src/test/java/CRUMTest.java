6f831607e40a695c0736c36d5b87fb165149eb85
tmack4444 <>
2021-05-07 15:06:14 -0400

    Working on testing initOSHI

--- src/test/java/CRUMTest.java
+++ src/test/java/CRUMTest.java
@@ -1,4 +1,23 @@
+import oshi.SystemInfo;
+import oshi.hardware.HWDiskStore;
+import oshi.hardware.HardwareAbstractionLayer;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CRUMTest {
-  
+    private static List<HWDiskStore> disks;
+    private static SystemInfo si;
+    private static HardwareAbstractionLayer hal;
+    private static String SerialNum;
+    public static int numDisks;
+  public static void initOSHItest(){
+      CRUM.initOSHI();
+      assertNotNull(si);
+      assertNotNull(hal);
+      assertNotNull(disks);
+      assertNotNull(SerialNum);
+  }
+
 }
