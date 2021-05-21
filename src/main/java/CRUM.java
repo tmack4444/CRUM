66c9686f75fa6d631fc79dfe8fb0dcb2a57855b1
tmack4444 <>
2021-05-21 14:56:57 -0400

    Converting used RAM to GB instead of just Bytes

--- src/main/java/CRUM.java
+++ src/main/java/CRUM.java
@@ -316,7 +316,7 @@ public class CRUM {
         smi.setLong(4, memory.getTotal()/1000000000);
         smi.setLong(5, totalPhysMem/1000000000);
         smi.setLong(6, memory.getVirtualMemory().getVirtualMax()/1000000000);
-        smi.setLong(7, usedMemory);
+        smi.setLong(7, usedMemory/1000000000);
         smi.execute();
         LOGGER.info("Num Mem Modules: {}", numMemModules);
         LOGGER.info("Total Memory:  {}", memory.getTotal());
